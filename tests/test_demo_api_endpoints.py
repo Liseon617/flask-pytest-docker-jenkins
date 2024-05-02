eacf42e3696bcdd23ca515100901bd7ec38db815
Liseon617 <blzl.l.gtsj@gmail.com>
2024-05-02 17:13:56 +0800

    completed the dockerfile + docker-compose

--- tests/test_demo_api_endpoints.py
+++ tests/test_demo_api_endpoints.py
@@ -2,14 +2,14 @@ from main import app
 
 def test_get_api_endpoint():
     with app.test_client() as c:
-        response = c.get('api/DemoApiEndpoint')
+        response = c.get('/api/DemoApiEndpoint')
         assert response.status_code == 200
         json_response = response.get_json()
         assert json_response == {'message': 'This is a response from a GET request.'}
 
 def test_post_api_endpoint():
     with app.test_client() as c:
-        response = c.post('api/DemoApiEndpoint', json={
+        response = c.post('/api/DemoApiEndpoint', json={
             "name": "Vincent"
         })
         assert response.status_code == 200
