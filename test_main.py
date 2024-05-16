7a729ff96feab739cf75e572d3356aded9d1d6e0
Liseon617 <blzl.l.gtsj@gmail.com>
2024-05-17 02:10:12 +0800

    update

--- test_main.py
+++ test_main.py
@@ -1,5 +1,10 @@
 from main import app
 
+def test_home_route():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert b"Hello, World!" in response.data
+
 def test_get_api_endpoint():
     with app.test_client() as c:
         response = c.get('/api/DemoApiEndpoint')
@@ -10,8 +15,8 @@ def test_get_api_endpoint():
 def test_post_api_endpoint():
     with app.test_client() as c:
         response = c.post('/api/DemoApiEndpoint', json={
-            "name": "Vincent"
+            "name": "Brian"
         })
         assert response.status_code == 200
         json_response = response.get_json()
-        assert json_response == {'message': 'Nice to meet you, Vincent!'}
+        assert json_response == {'message': 'Nice to meet you, Brian!'}
